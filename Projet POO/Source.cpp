e1304d592cefb034a50fc4a42a99622dbac7e995
Ilias TRAGHA <104205016+BDF172@users.noreply.github.com>
2023-12-04 16:03:23 +0100

    Fin des requêtes créées directement dans le code

--- Projet POO/Source.cpp
+++ Projet POO/Source.cpp
@@ -1,10 +1,14 @@
 #include "headerLink.h"
 
 using namespace System::Windows::Forms;
+using namespace NS_composants;
 
 System::Void PageMain(System::Void) {
 	//ProjetPOO::principal^ pageAccueil = gcnew ProjetPOO::principal;
 	//Application::EnableVisualStyles();
 	//Application::Run(pageAccueil);
-	NS_Composants::
+	CL_Com_mapping_articles map;
+	//map.setID("1");
+	map.setPrix("24");
+	System::Console::WriteLine(map.SEARCH());
 }
