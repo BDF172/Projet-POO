34335282e619eb268c731af952d4f46235a9c8cb
Tom PEREZ LE TIEC <tom.perezletiec@viacesi.fr>
2023-12-04 09:57:30 +0100

    Ajout de la page principale

--- Projet POO/headerLink.h
+++ Projet POO/headerLink.h
@@ -2,7 +2,8 @@
 
 #include <vector>
 
+#include "gestion_personnel.h"
+#include "principal.h"
 #include "CL_CAD.h"
 #include "CL_Com_mapping.h"
-#include "MyForm.h"
 #include "CL_Com_mapping_personnel.h"
